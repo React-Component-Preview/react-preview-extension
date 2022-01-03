275f6ebb4a673d3d9227d9cdbc2013ac4105f97c
Seo Jun Yoo <sytwoyou@gmail.com>
2022-01-04 03:56:21 +0900

    Feat: add iframe preview to extension webview

---
+++ app/components/ControlMenu.tsx
@@ -0,0 +1,13 @@
+import React, { ReactElement } from "react";
+
+interface Props {}
+
+function ControlMenu({}: Props): ReactElement {
+  return(
+    <div>
+      I am Control Menu
+    </div>
+  );
+}
+
+export default ControlMenu;
