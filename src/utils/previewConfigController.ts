ffd1798a8d6095a4c4ed3e3e0ebb626d70828b55
Seo Jun Yoo <sytwoyou@gmail.com>
2022-01-13 04:24:43 +0900

    Fix: change filename from propsRecord to previewConfigController & fs async to sync methods

---
+++ src/utils/previewConfigController.ts
@@ -0,0 +1,60 @@
+import * as path from "path";
+import * as fs from "fs";
+
+import { Prop, PreviewConfig } from "../types";
+
+export const addGitIgnore = (workspacePath: string) => {
+  const gitIgnoreContent = "\n# React Component Preview\npreviewConfig.json";
+
+  if (fs.existsSync(path.join(workspacePath, ".gitignore"))) {
+    const gitIgnore = fs.readFileSync(path.join(workspacePath, ".gitignore"), "utf8");
+
+    if (gitIgnore.indexOf(gitIgnoreContent) === -1) {
+      fs.appendFileSync(path.join(workspacePath, ".gitignore"), gitIgnoreContent);
+    }
+  }
+};
+
+export const createAndShowPreviewConfig = (workspacePath: string) => {
+  let previewConfigData: PreviewConfig = {};
+
+  if (!fs.existsSync(path.join(workspacePath, "previewConfig.json"))) {
+    fs.appendFileSync(path.resolve(workspacePath, "previewConfig.json"), "");
+  } else {
+    const previewConfig = fs.readFileSync(
+      path.resolve(workspacePath, "previewConfig.json"),
+      "utf8",
+    );
+
+    previewConfigData = previewConfig ? JSON.parse(previewConfig) : {};
+  }
+
+  return previewConfigData;
+};
+
+export const addPropInfo = (workspacePath: string, componentName: string, propInfo: Prop) => {
+  const previewConfigData = createAndShowPreviewConfig(workspacePath);
+
+  previewConfigData[componentName] = previewConfigData[componentName] || [];
+  previewConfigData[componentName].push(propInfo);
+
+  fs.writeFileSync(
+    path.resolve(workspacePath, "previewConfig.json"),
+    JSON.stringify(previewConfigData, null, 2),
+  );
+};
+
+export const removePropInfo = (workspacePath: string, componentName: string, propName: string) => {
+  const previewConfigData = createAndShowPreviewConfig(workspacePath);
+
+  if (previewConfigData[componentName]) {
+    previewConfigData[componentName] = previewConfigData[componentName].filter(
+      (prop: Prop) => prop.propName !== propName,
+    );
+  }
+
+  fs.writeFileSync(
+    path.resolve(workspacePath, "previewConfig.json"),
+    JSON.stringify(previewConfigData, null, 2),
+  );
+};
