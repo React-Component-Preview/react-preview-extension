ef503a125a77d0ecbc61f21203ca894e002dcb85
Seo Jun Yoo <sytwoyou@gmail.com>
2022-01-15 01:33:54 +0900

    Fix: webview close when vscode closes

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,6 @@
 import * as vscode from "vscode";
 
 import WebviewPanel from "./WebviewPanel";
-import getWebviewOptions from "./utils/getWebviewOptions";
 import { addGitIgnore } from "./utils/previewConfigController";
 
 export const activate = (context: vscode.ExtensionContext) => {
@@ -18,21 +17,6 @@ export const activate = (context: vscode.ExtensionContext) => {
       WebviewPanel.createAndShow(context.extensionUri, workspacePath);
     }),
   );
-
-  if (vscode.window.registerWebviewPanelSerializer) {
-    vscode.window.registerWebviewPanelSerializer(WebviewPanel.viewType, {
-      async deserializeWebviewPanel(webviewPanel: vscode.WebviewPanel) {
-        const workspaceFolders = vscode.workspace.workspaceFolders;
-
-        if (!workspaceFolders) return;
-
-        const workspacePath = workspaceFolders[0].uri.path;
-
-        webviewPanel.webview.options = getWebviewOptions(context.extensionUri);
-        WebviewPanel.revive(webviewPanel, context.extensionUri, workspacePath);
-      },
-    });
-  }
 };
 
 export const deactivate = () => {
